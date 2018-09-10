dbd24b82c28bfdb164221a24617211c7da801a13
Saravanakumar Sukumar <saravanakumar1210@gmail.com>
2018-09-10 19:45:43 +0100

    Updated with cardholder name and tokenization key replaced with client token

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/test.html', indexRouter);
+app.use('/landingPageCT.html', indexRouter);
 app.use('/users', usersRouter);
 app.use('/checkout', checkout);
 // catch 404 and forward to error handler
--- routes/checkout.js
+++ routes/checkout.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var braintree = require('braintree');
 
-router.post('/', function(req, res, next) {
-  var gateway = braintree.connect({
+var gateway = braintree.connect({
     environment: braintree.Environment.Sandbox,
     // Use your own credentials from the sandbox Control Panel here
     merchantId: '3xj4zw2pjhs26929',
@@ -11,9 +10,26 @@ router.post('/', function(req, res, next) {
     privateKey: 'c3c33d8fb55cd194145efc975b8828a3'
   });
 
+  //generate a client token -ssk
+  gateway.clientToken.generate({
+	  customerId: 887631396
+	}, function (err, response) {
+	  var clientToken = response.clientToken
+	});
+	
+	//for get methods
+	router.get('/', function(req, res, next) {
+		gateway.clientToken.generate({}, function (err, response) {
+			res.send(response.clientToken);
+		}); 
+	});
+
+
+//post methods
+router.post('/checkout', function(req, res, next) {
   // Use the payment method nonce here
-  var nonceFromTheClient = req.body.paymentMethodNonce;
-  // Create a new transaction for $10
+ 	 var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for 10 Euros
   var newTransaction = gateway.transaction.sale({
     amount: '10.00',
     paymentMethodNonce: nonceFromTheClient,
